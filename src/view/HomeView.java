86e9df71a96d99a111524e374541651e6b9258bb
zyyx-toandv <toandv@zyyx.jp>
2024-10-21 17:52:52 +0700

    layout basic

--- src/view/HomeView.java
+++ src/view/HomeView.java
@@ -1,20 +1,57 @@
 package view;
 
+import com.formdev.flatlaf.FlatClientProperties;
 import main.Application;
+import net.miginfocom.layout.ComponentWrapper;
+import net.miginfocom.layout.LayoutCallback;
+import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 
-public class HomeView extends JPanel {
+// UI description:
+// 1/3 left: user info: avatar, username, rank, elo, total games, win rate, history button, logout button
+// 2/3 right: ranking table: index, name, elo, total games, win rate, invite button
+public class HomeView extends JPanel implements ComponentResizeListener {
     public HomeView() {
         setName("Home View");
-        setLayout(null);
+        setLayout(new MigLayout("fill, insets 16", "[]16[]", ""));
 
-        // back button
-        JButton button = new JButton("Back");
-        button.setBounds(10, 40, 100, 20);
-        button.addActionListener(e -> {
-            Application.getInstance().back();
-        });
-        add(button);
+        initComponents();
+        initEvents();
+
+        Application.getInstance().componentResizeListener = this;
+    }
+
+    private void initComponents() {
+        initUserInfo();
+        initRankingTable();
+    }
+
+    private void initUserInfo() {
+        JPanel userInfoPanel = new JPanel();
+        userInfoPanel.setLayout(new MigLayout("fill, insets 16, wrap"));
+        userInfoPanel.putClientProperty(FlatClientProperties.STYLE, "" +
+                "background:darken(@background, 5%);" +
+                "arc:16");
+        add(userInfoPanel, "grow, hidemode 3");
+    }
+
+    private void initRankingTable() {
+        JTable rankingTable = new JTable();
+        JScrollPane rankingScrollPane = new JScrollPane(rankingTable);
+        add(rankingScrollPane, "span 2, grow");
+    }
+
+    private void initEvents() {
+
+    }
+
+    @Override
+    public void componentResized(ComponentEvent e) {
+        System.out.println("Home View resized");
     }
 }
